adeb9e3b83ae3c4ffcad81055f8bb5395301a1c5
microvnn@gmail.com <microvnn@gmail.com>
2020-09-30 09:24:09 +0700

    clean temp

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ import io
 import os
 from setuptools import setup
 
-# Use the VERSION file to get version
-version_file = os.path.join(os.path.dirname(__file__), "language_vn", "VERSION")
-with open(version_file) as fh:
-    version = fh.read().strip()
-
 install_requires = [
     "Click>=6.0",
     "python-crfsuite==0.9.6",
@@ -20,10 +15,10 @@ setup_requires = []
 
 setup(
     name="language_vn",
-    version=version,
+    version="1.0.0",
     description="Vietnamese NLP Toolkit",
-    author="Vu Anh",
-    author_email="anhv.ict91@gmail.com",
+    author="hungth",
+    author_email="microvnn@gmail.com",
     url="https://github.com/microvnn/language_vn",
     packages=["language_vn",],
     package_dir={"language_vn": "language_vn"},
